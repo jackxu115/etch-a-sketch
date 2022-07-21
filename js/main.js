925cf6584b7e8eb6a143929141a660bea676022e
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-07-21 01:19:01 -0700

    implement draw function

--- js/main.js
+++ js/main.js
@@ -1,40 +1,52 @@
 const sizeBtn = document.querySelector("#sizeBtn")
+const blackBtn = document.querySelector("#blackBtn")
+const rgbBtn = document.querySelector("#sizeBtn")
+const colorBtn = document.querySelector('#colorBtn')
+const eraserBtn = document.querySelector("#eraserBtn")
+const clearBtn = document.querySelector("#clearBtn")
 const size = document.querySelector("#size")
+const grid = document.querySelector("#grid")
+let defaultColor = 'black'
 
-const sizeBar = document.createElement('div')
-const sizeBarInput = document.createElement('input')
-const sizeTag = document.createElement('p')
+
+const div = document.createElement('div')
+const input = document.createElement('input')
+const p = document.createElement('p')
 
 sizeBtn.addEventListener('mouseover', () => {
     showSizeBar()
     getSizeValue()
     showSizeValue()
-    createGridItem(3)
 })
 
-sizeBarInput.addEventListener('change', () =>{
+input.addEventListener('change', () =>{
     showSizeValue()
+    grid.innerHTML = ''
+    displayGrid(getSizeValue())
+    document.querySelectorAll('.cell').forEach(item => {
+        item.addEventListener('mouseover', () => draw(defaultColor, item))
+    })
 })
 
-sizeBarInput.addEventListener('mouseleave', () => removeSizeBar())
+input.addEventListener('mouseleave', () => removeSizeBar())
 
 const removeSizeBar = () => {
-    size.removeChild(sizeBar)
+    size.removeChild(div)
 }
 
 const setSizeBarAttribute = () => {
-    sizeBarInput.setAttribute('id', 'slider')
-    sizeBarInput.setAttribute('type', 'range')
-    sizeBarInput.setAttribute('style','margin: 0; padding: 0')
-    sizeBarInput.setAttribute('min', '1')
-    sizeBarInput.setAttribute('max', '100')
-    sizeBarInput.setAttribute('value','16')
+    input.setAttribute('id', 'slider')
+    input.setAttribute('type', 'range')
+    input.setAttribute('style','margin: 0; padding: 0')
+    input.setAttribute('min', '1')
+    input.setAttribute('max', '100')
+    input.setAttribute('value','16')
 }
 
 const showSizeBar = () => {
     setSizeBarAttribute()
-    size.appendChild(sizeBar)
-    sizeBar.appendChild(sizeBarInput)
+    size.appendChild(div)
+    div.appendChild(input)
 
 }
 
@@ -44,18 +56,57 @@ const getSizeValue = () => {
 }
 
 const showSizeValue = () => {
-    sizeTag.setAttribute('id', 'sizeValue')
-    sizeTag.setAttribute('style', 'margin: 0')
-    sizeTag.textContent = getSizeValue()
-    sizeBar.appendChild(sizeTag)
+    p.setAttribute('id', 'sizeValue')
+    p.setAttribute('style', 'margin: 0')
+    p.textContent = getSizeValue()
+    div.appendChild(p)
 
 }
 
 const createGridItem = num => {
-    const maxNum = num * num
-    const gridItem = new Array(maxNum).fill('<div class="cell"></div>')
-    console.log(gridItem[0])
+    const div = document.createElement('div')
+    div.setAttribute('class','cell')
+    div.setAttribute('style', 'background: #fff; border: 1px solid #000')
+    return div
 }
+
+const displayGrid = (num) => {
+    const rowAttribute = `grid-template-rows: repeat(${num}, 2fr)`
+    const columnAttribute = `grid-template-columns: repeat(${num}, 2fr)`
+    grid.setAttribute('style', `${rowAttribute};${columnAttribute}`)
+    const maxNum = num * num
+    let div
+    for (let i = 0; i < maxNum; i++ ) {
+        div = createGridItem()
+        grid.appendChild(div)
+    }
+}
+
+blackBtn.addEventListener('click', () => defaultColor = "#000")
+
+rgbBtn.addEventListener('click', () => {
+
+})
+
+const setColorPicker = () => {
+    input.setAttribute('type', 'color')
+    input.setAttribute('id', 'colorItem')
+}
+
+
+colorBtn.addEventListener('click', () => {
+
+})
+
+
+const draw = (color, item) => {
+    item.setAttribute('style', `background: ${color}`)
+}
+
+
+
+
+
 
 
 
